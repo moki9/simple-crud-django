970e660f6ec031d76fa131bc5e5387e01b01bbe8
Louie Tanalas <louie@imfreemobile.com>
2016-06-24 15:36:33 +0800

    added delete

--- friend/urls.py
+++ friend/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^$', FriendList.as_view(), name='friend_list'),
     url(r'^create/$', FriendCreate.as_view(), name='friend_create'),
     url(r'^(?P<pk>\d+)/edit/$', FriendUpdate.as_view(), name='friend_edit'),
-    url(r'^(?P<pk>\d+)delete/$', FriendDelete.as_view(), name='friend_delete'),
+    url(r'^(?P<pk>\d+)/delete/$', FriendDelete.as_view(), name='friend_delete'),
     ]
